db79598f31736daa049dab28260f1b1e93763ca4
ARYAN NIPANE <63520003+aaryannipane@users.noreply.github.com>
2022-10-12 17:04:51 +0530

    added axios

---
+++ frontend/src/http/index.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true,
+  headers: {
+    "Content-type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+// list of all endpoints
+
+// logins
+export const adminLogin = (data) => api.post("/api/login-admin", data);
+export const nurseLogin = (data) => api.post("/api/login-nurse", data);
+
+export default api;
