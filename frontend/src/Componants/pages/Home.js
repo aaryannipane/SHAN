e52935286b17a615c7b5b4f5657cd878e394c1a1
chandanrathod4512@gmail.com <chandya968@gmail.com>
2023-02-06 20:44:41 -0800

    changes

--- frontend/src/Componants/pages/Home.js
+++ frontend/src/Componants/pages/Home.js
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
 import { nurseLogin } from "../../http";
-
+import {useNavigate} from "react-router-dom";
 export const Home = () => {
+  const navigate  = useNavigate()
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+    
 
+  // function userHandler(e){
+  // let item=e.target.value;
+  // if(item.length<3){
+  //   setUsername
+  // }
+  // }
   return (
     <>
       <form>
         <input
-          type="username"
+          required
+          type="email"
           placeholder="username"
           onChange={(e) => setUsername(e.currentTarget.value)}
         />
         <input
+          required
           type="password"
           placeholder="password"
           onChange={(e) => setPassword(e.currentTarget.value)}
         />
         <button
+        type="submit"
           onClick={async (e) => {
             e.preventDefault();
             const result = await nurseLogin({
@@ -26,7 +38,9 @@ export const Home = () => {
               password: password,
             });
 
-            console.log(result);
+            navigate("/new")
+            localStorage.setItem("auth",JSON.stringify(result));
+          //  console.log(result);
           }}
         >
           Login
