a7c8c5d01876d4e819f7b31d571efdf2f1c589a6
aaryannipane <nipaneeducation@gmail.com>
2023-02-08 13:27:32 +0530

    patient db completed

--- backend/models/patientModel.js
+++ backend/models/patientModel.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
-// TODO: add values in background component
 const PatientSchema = new mongoose.Schema(
   {
     mrNo: { type: Number, unique: true, required: true },
@@ -120,22 +119,21 @@ const PatientSchema = new mongoose.Schema(
         singleDonorPlateletes: { type: Number, default: 0 },
         cryoprecipitateAntihemophilicFactor: { type: Number, default: 0 },
       },
-      // TODO: add values
       skinImpairment: {
         bedSore: {
           stage: { type: String, enum: ["present", "absent"] },
           oozing: { type: String, enum: ["done", "notDone"] },
-          positioning: { type: String, enum: [] },
+          positioning: { type: String, enum: ["given", "notGiven"] },
         },
         wound: {
           stage: { type: String, enum: ["present", "absent"] },
           oozing: { type: String, enum: ["done", "notDone"] },
-          positioning: { type: String, enum: [] },
+          positioning: { type: String, enum: ["given", "notGiven"] },
         },
         burn: {
           stage: { type: String, enum: ["present", "absent"] },
           oozing: { type: String, enum: ["done", "notDone"] },
-          positioning: { type: String, enum: [] },
+          positioning: { type: String, enum: ["given", "notGiven"] },
         },
       },
       totalHemodialysis: {
@@ -147,35 +145,258 @@ const PatientSchema = new mongoose.Schema(
         temperature: { type: Number },
         pulse: { type: Number },
         respiration: { type: Number },
-        // TODO: add bp two parameters correct
-        bloodPressure: { up: { type: Number }, down: { type: Number } },
-        spo2: {
-          type: Number,
+        bloodPressure: {
+          sistolic: { type: Number },
+          distolic: { type: Number },
         },
-        otherParameters: {
-          etco2: { type: String },
-          cvp: { type: String },
-          abp: { type: String },
+      },
+      spo2: {
+        type: Number,
+      },
+      otherParameters: {
+        etco2: { type: String },
+        cvp: { type: String },
+        abp: { type: String },
+      },
+      ecg: {
+        type: String,
+        enum: ["sr", "sb", "st"],
+      },
+      arrhythmia: {
+        type: String,
+        enum: ["svt", "af", "af1", "vTach", "vf"],
+      },
+      invasiveLines: {
+        veinFlow: {
+          doi: { type: String },
+          size: { type: String },
+          site: { type: String },
+          dod: { type: String },
+          dor: { type: String },
         },
-        ecg: {
-          type: String,
-          enum: ["sr", "sb", "st"],
+        centralLine: {
+          doi: { type: String },
+          size: { type: String },
+          site: { type: String },
+          dod: { type: String },
+          dor: { type: String },
         },
-        arrhythmia: {
-          type: String,
-          enum: ["svt", "af", "af1", "vTach", "vf"],
+        arterialLine: {
+          doi: { type: String },
+          size: { type: String },
+          site: { type: String },
+          dod: { type: String },
+          dor: { type: String },
         },
-        // TODO: what to choose better for update and read
-        invasiveLines: {
-          veinFlow: {
-            doi: { type: String },
-            size: { type: String },
-            site: { type: String },
-            dod: { type: String },
-            dor: { type: String },
+        hdCatheter: {
+          doi: { type: String },
+          size: { type: String },
+          site: { type: String },
+          dod: { type: String },
+          dor: { type: String },
+        },
+        rylesTube: {
+          doi: { type: String },
+          size: { type: String },
+          site: { type: String },
+          dod: { type: String },
+          dor: { type: String },
+        },
+        foleysCatheter: {
+          doi: { type: String },
+          size: { type: String },
+          site: { type: String },
+          dod: { type: String },
+          dor: { type: String },
+        },
+        endotrachealTube: {
+          doi: { type: String },
+          size: { type: String },
+          site: { type: String },
+          dod: { type: String },
+          dor: { type: String },
+        },
+        tracheostomyTube: {
+          doi: { type: String },
+          size: { type: String },
+          site: { type: String },
+          dod: { type: String },
+          dor: { type: String },
+        },
+        intercostalDrain: {
+          doi: { type: String },
+          size: { type: String },
+          site: { type: String },
+          dod: { type: String },
+          dor: { type: String },
+        },
+        other: {
+          doi: { type: String },
+          size: { type: String },
+          site: { type: String },
+          dod: { type: String },
+          dor: { type: String },
+        },
+      },
+      scales: {
+        gcs: {
+          eyes: {
+            type: Number,
           },
-          centralLine: [{ type: String }],
+          verbalResponse: {
+            type: Number,
+          },
+          motorResponse: {
+            type: Number,
+          },
+          total: {
+            type: Number,
+          },
         },
+        numericRatingScale: {
+          type: Number,
+          min: 0,
+          max: 10,
+        },
+        bradenScale: {
+          sensoryPerception: {
+            type: Number,
+          },
+          moisture: {
+            type: Number,
+          },
+          activity: {
+            type: Number,
+          },
+          mobility: {
+            type: Number,
+          },
+          nutrition: {
+            type: Number,
+          },
+          frictionAndShear: {
+            type: Number,
+          },
+          total: {
+            type: Number,
+          },
+        },
+        fallRiskAssessmentScore: {
+          historyOfFall: { type: Number, enum: [0, 25] },
+          secondaryDiagnosis: { type: Number, enum: [0, 25] },
+          ambulatoryAid: { type: Number, enum: [0, 15, 30] },
+          iv: { type: Number, enum: [0, 20] },
+          gait: { type: Number, enum: [0, 10, 20] },
+          mentalStatus: { type: Number, enum: [0, 15] },
+        },
+        vip: {
+          type: Number,
+          min: 0,
+          max: 5,
+        },
+        cpot: {
+          facialExpression: {
+            type: Number,
+          },
+          bodyMovements: {
+            type: Number,
+          },
+          muscleTension: {
+            type: Number,
+          },
+          complianceWithTheVentilator: {
+            type: Number,
+          },
+          vocalization: {
+            type: Number,
+          },
+        },
+      },
+      intake: {
+        oral: { type: Number },
+        rt: { type: Number },
+        iv: { type: Number },
+        other: { type: Number },
+        total: { type: Number },
+      },
+      output: {
+        urine: { type: Number },
+        rta: { type: Number },
+        drain: { type: Number },
+        other: { type: Number },
+        total: { type: Number },
+      },
+      investigations: {
+        pathology: {
+          randomBloodSugar: {
+            type: Number,
+          },
+          hemoglobin: {
+            type: Number,
+          },
+          wbc: {
+            type: Number,
+          },
+          rbc: {
+            type: Number,
+          },
+          plateletCount: {
+            type: Number,
+          },
+          creatine: {
+            type: Number,
+          },
+          sodium: {
+            type: Number,
+          },
+          potassium: {
+            type: Number,
+          },
+          chloride: {
+            type: Number,
+          },
+          abg: {
+            type: Number,
+          },
+          hhh: {
+            type: Number,
+          },
+          lft: {
+            type: Number,
+          },
+          coagulationProfile: {
+            type: Number,
+          },
+          other: {
+            type: String,
+          },
+        },
+        radiology: {
+          xray: { type: String },
+          ctScan: { type: String },
+          mri: { type: String },
+          other: { type: String },
+        },
+      },
+    },
+    recommendation: {
+      pendingMedication: {
+        type: String,
+      },
+      pendingReports: {
+        type: String,
+      },
+      references: {
+        type: String,
+      },
+      specialOrder: {
+        type: String,
+      },
+      pendingProcedures: {
+        type: String,
+      },
+      otherOrder: {
+        type: String,
       },
     },
   },
