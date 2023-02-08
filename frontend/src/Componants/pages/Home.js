ff9769e9b26b69ef6d572fdb64646374b2a4d5ca
chandanrathod4512@gmail.com <chandya968@gmail.com>
2023-02-08 01:05:43 -0800

    added validation

--- frontend/src/Componants/pages/Home.js
+++ frontend/src/Componants/pages/Home.js
@@ -1,51 +1,86 @@
 import React, { useState } from "react";
 import { nurseLogin } from "../../http";
 import {useNavigate} from "react-router-dom";
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 export const Home = () => {
   const navigate  = useNavigate()
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [usernamerr, setUsernameerr] = useState("");
+  const [passworderr, setPassworderr] = useState("");
     
+  async function  loginHandler(e)
+   {
+    e.preventDefault();
+    
+    if(username===""||password==="")
+    {
+      return alert("type correct values")
+    }
+      
+      const result = await nurseLogin({
+        username: username,
+        password: password,
+      });
+      console.log(result);
+      navigate("/SelectHospital")
+       
+   }
+  
 
-  // function userHandler(e){
-  // let item=e.target.value;
-  // if(item.length<3){
-  //   setUsername
-  // }
-  // }
+  function userHandler(e){
+  let item=e.target.value;
+  if(item.length<3){
+    setUsernameerr(true)
+  }
+  else{
+    setUsernameerr(false)
+  }
+setUsername(item)
+}
+  function passHandler(e){
+    let item=e.target.value;
+    if(item.length<3){
+      setPassworderr(true)
+    }
+    else{
+      setPassworderr(false)
+    }
+    setPassword(item)
+    }
   return (
     <>
-      <form>
-        <input
-          required
+    <Form >
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control required
           type="email"
           placeholder="username"
-          onChange={(e) => setUsername(e.currentTarget.value)}
-        />
-        <input
-          required
+          onChange={userHandler} />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group> {usernamerr?<span>User Not Valid</span>:""}
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control required
           type="password"
           placeholder="password"
-          onChange={(e) => setPassword(e.currentTarget.value)}
-        />
+          onChange={passHandler}/>
+      </Form.Group>{passworderr?<span>Password Not Valid</span>:""}
+     
+      
         <button
         type="submit"
-          onClick={async (e) => {
-            e.preventDefault();
-            const result = await nurseLogin({
-              username: username,
-              password: password,
-            });
-
-            navigate("/new")
-            localStorage.setItem("auth",JSON.stringify(result));
-          //  console.log(result);
-          }}
+          onClick={loginHandler}
         >
           Login
         </button>
-      </form>
+      </Form>
     </>
   );
 };
--- frontend/src/store/UserSlice.js
+++ frontend/src/store/UserSlice.js
@@ -9,15 +9,7 @@ export const userSlice = createSlice({
     name:"user",
     initialState,
     reducers: {
-        setUser: (state, action)=>{
-            const user = action.payload;
-            if(!user){
-                state.isAuth = false;
-            }
-
-            action.isAuth = true
-
-        }
+         
     }
 })
 
